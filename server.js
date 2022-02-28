ce2c3fb40ff2e13b5c1c2e882a012b9b0112b3a5
Vinay Kumar <mailbox4vinay@gmail.com>
2022-02-28 18:00:35 +0530

    added post mothod to print payload

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.post('/notify', (req, res) => {
+    console.log(req.body);
+  });
+
 app.get('/', (req, res) => {
   res.send('Successful response.');
 });
